28ea60835b2cf1267cd301d759e61ed5a94f2a2a
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 20:42:12 -0600

    Finalizado Ejercicio6

--- Ejercicio6/src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
+++ Ejercicio6/src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
@@ -1,17 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ejercicio6;
-
-/**
- *
- * @author josep
- */
-public class Ejercicio6 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.swing.*;
+import javax.swing.event.*;
+public class Ejercicio6 extends JFrame implements ChangeListener {
+    private JCheckBox check1, check2,check3;
+    public Ejercicio6() {
+        setLayout(null);
+        check1=new JCheckBox("Ingles");
+        check1.setBounds(10, 10, 150, 30);
+        check1.addChangeListener(this);
+        add(check1);
+        check2=new JCheckBox("Frances");
+        check2.setBounds(10, 50, 150, 30);
+        check2.addChangeListener(this);
+        add(check2);
+        check3=new JCheckBox("Aleman");
+        check3.setBounds(10, 90, 150, 30);
+        check3.addChangeListener(this);
+        add(check3);
+        
     }
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        String cad="";
+        if (check1.isSelected()==true){
+            cad=cad+"Ingles-";
+        }
+        if (check2.isSelected()==true){
+            cad=cad+"Frances-";
+        }
+        if (check3.isSelected()==true){
+            cad=cad+"Aleman-";
+        }
+        setTitle(cad);
+    }
+    public static void main(String[] args) {
+        Ejercicio6 formulario1=new Ejercicio6();
+        formulario1.setBounds(0, 0, 300, 200);
+        formulario1.setVisible(true);
+    }
+
+    
 }
