689f9e85d3705c5a1bd21ad3ca6b856ceff36834
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 19:19:27 -0600

    Ejercicio1 finalizado

--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -1,17 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ejercicio1;
-
-/**
- *
- * @author josep
- */
-public class Ejercicio1 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.swing.*;
+import java.awt.event.*;
+public class Ejercicio1 extends JFrame implements ActionListener{
+    private JScrollPane scrollpanel;
+    private JTextArea textarea1;
+    private JButton boton1;
+    public Ejercicio1(){
+        setLayout(null);
+        textarea1=new JTextArea();
+        scrollpanel=new JScrollPane(textarea1);
+        scrollpanel.setBounds(10, 10, 300, 200);
+        add(scrollpanel);
+        boton1=new JButton("Verificar");
+        boton1.setBounds(10, 260, 100, 30);
+        add(boton1);
+        boton1.addActionListener(this);
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==boton1){
+            String texto=textarea1.getText();
+            if (texto.indexOf("argentina")==-1){
+                setTitle("No contiene el texto \"argentina\"");
+            } else {
+                setTitle("Si contiene el texto \"argentina\"");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Ejercicio1 formulario1=new Ejercicio1();
+        formulario1.setBounds(0, 0, 400, 380);
+        formulario1.setVisible(true);
+    }
+
+    
 }
