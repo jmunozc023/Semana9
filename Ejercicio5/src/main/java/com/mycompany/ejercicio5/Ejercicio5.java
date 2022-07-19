812a6b258aa4a871fb3638c484822d737d9a8586
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 20:24:26 -0600

    Update Ejercicio5.java

--- Ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
@@ -1,17 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ejercicio5;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class Ejercicio5 extends JFrame implements ActionListener {
+    private JMenuBar mb;
+    private JMenu menu1, menu2,menu3;
+    private JMenuItem mi1,mi2,mi3,mi4;
+    
+    public Ejercicio5(){
+        setLayout(null);
+        mb=new JMenuBar();
+        setJMenuBar(mb);
+        menu1=new JMenu("Opciones");
+        mb.add(menu1);
+        menu2=new JMenu("Tamaño de la ventana");
+        menu1.add(menu2);
+        menu3=new JMenu("Color de fondo");
+        menu1.add(menu3);
+        mi1=new JMenuItem("640*480");
+        menu2.add(mi1);
+        mi1.addActionListener(this);
+        mi2=new JMenuItem("1024*768");
+        menu2.add(mi2);
+        mi2.addActionListener(this);
+        mi3=new JMenuItem("Rojo");
+        menu3.add(mi3);
+        mi3.addActionListener(this);
+        mi4=new JMenuItem("Verde");
+        menu3.add(mi4);
+        mi4.addActionListener(this);
 
-/**
- *
- * @author josep
- */
-public class Ejercicio5 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    public static void main(String[] args) {
+        
+    }
+
+    
 }
