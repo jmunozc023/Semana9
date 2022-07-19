868c45272f39fd2f0c86113796fcd986afade734
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 21:20:05 -0600

    Finalizado Ejercicio8

--- Ejercicio8/src/main/java/com/mycompany/ejercicio8/Ejercicio8.java
+++ Ejercicio8/src/main/java/com/mycompany/ejercicio8/Ejercicio8.java
@@ -1,17 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ejercicio8;
-
-/**
- *
- * @author josep
- */
-public class Ejercicio8 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.swing.*;
+import javax.swing.event.*;
+public class Ejercicio8 extends JFrame implements ChangeListener{
+    private JRadioButton radio1,radio2,radio3;
+    private ButtonGroup bg;
+    public Ejercicio8() {
+        setLayout(null);
+        bg=new ButtonGroup();
+        radio1=new JRadioButton("640*480");
+        radio1.setBounds(10, 20, 100, 30);
+        radio1.addChangeListener(this);
+        add(radio1);
+        bg.add(radio1);
+        radio2=new JRadioButton("800*600");
+        radio2.setBounds(10, 70, 100, 30);
+        radio2.addChangeListener(this);
+        add(radio2);
+        bg.add(radio2);
+        radio3=new JRadioButton("1024*768");
+        radio3.setBounds(10, 120, 100, 30);
+        radio3.addChangeListener(this);
+        add(radio3);
+        bg.add(radio3);
     }
+    
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if (radio1.isSelected()){
+            setSize(640,480);
+        }
+        if (radio2.isSelected()){
+            setSize(800,600);
+        }
+        if (radio3.isSelected()){
+            setSize(1024,768);
+        }
+    }
+    public static void main(String[] args) {
+        Ejercicio8 formulario1=new Ejercicio8();
+        formulario1.setBounds(0, 0, 350, 230);
+        formulario1.setVisible(true);
+    }
+
+    
 }
