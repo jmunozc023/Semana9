85032f5d8ee62f2645e682e5d8a94dad1fb1b5a9
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-18 19:31:50 -0600

    Finalizado Ejercicio2

--- Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
+++ Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -1,17 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.ejercicio2;
 
-/**
- *
- * @author josep
- */
-public class Ejercicio2 {
+import java.awt.event.*;
+import javax.swing.*;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+public class Ejercicio2 extends JFrame implements ItemListener{
+    private JComboBox combo1;
+    public Ejercicio2(){
+        setLayout(null);
+        combo1=new JComboBox();
+        combo1.setBounds(10, 10, 150, 20);
+        add(combo1);
+        combo1.addItem("rojo");
+        combo1.addItem("verde");
+        combo1.addItem("azul");
+        combo1.addItem("amarillo");
+        combo1.addItem("negro");
+        combo1.addItemListener(this);
     }
+    
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (e.getSource()==combo1){
+            String seleccionado=(String)combo1.getSelectedItem();
+            setTitle(seleccionado);
+        }
+    }
+    public static void main(String[] args) {
+        Ejercicio2 formulario1=new Ejercicio2();
+        formulario1.setBounds(0, 0, 400, 200);
+        formulario1.setVisible(true);
+    }
+
+    
 }
